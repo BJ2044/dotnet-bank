d25aad58c8674f1baacd672206fd54661df115ae
Brad Jones <BJ2044@gmail.com>
2022-11-14 14:46:31 -0500

    Get Balance

--- program.cs
+++ program.cs
@@ -1,1 +1,14 @@
-Console.WriteLine("hello world");
+Console.WriteLine("Welcome to the Bank");
+
+BankAccount account = new BankAccount(11000);
+
+// BankAccount account = new BankAccount("New account namme");
+try {
+account.makeDeposit(-1);
+} catch(Exception ex) {
+  Console.WriteLine(ex.Message);
+}
+// account.balance = 100.97m;
+
+Console.WriteLine($" Your balance is: {account.getBalance()} \n Account Number: {account.accountNumber} \n");
+Console.WriteLine($" Account Number: {account.accountNumber}");
